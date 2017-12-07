475db1c327c4cebab9887b38bd696d4c28d104bc
unknown <aguirreh8@gmail.com>
2017-12-06 21:40:21 -0500

    defined resetWordDisplay and checkLetterMatch functions, changed wordDisplay to array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,29 @@ let wins = 0;
 let losses = 0;
 let tries = 9;
 let currentWord = "";
-let wordDisplay = "";
+let wordDisplay = [];
 
 function pickWord() {
 	currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 }
+
+function resetWordDispaly(x) {
+	for (let i = 0; i < x.length; i++) {
+		wordDisplay[i] = " _ ";
+	};
+}
+
+function checkLetterMatch(y) {
+	for (let i = 0; i < currentWord.length; i++) {
+		if (y === currentWord.charAt(i)) {
+			wordDisplay[i] = y;
+		}
+	}
+}
+
+// pickWord();
+console.log(currentWord);
+resetWordDispaly(currentWord);
+console.log(wordDisplay);
+checkLetterMatch('p');
+console.log(wordDisplay);
